f1fd4a1f464824f0e7d583feb8da3155b11c6166
Bartłomiej Dziwoń <dziwon.bartlomiej@gmail.com>
2017-01-23 21:02:22 +0100

    Automatyczne rozmieszczenie kodu jak się nie chce wcięć robić !

--- src/main/java/ProjectBuilder.java
+++ src/main/java/ProjectBuilder.java
@@ -1,36 +1,34 @@
-
 public class ProjectBuilder {
 
-	private Project project=new Project();
-	
+    private Project project = new Project();
 
-	public Project build(){
-	return this.project;
-	}
-	
-	public ProjectBuilder(){
-	this.project=build();	
-	}
-	
-	public ProjectBuilder setTitle(String title){
-	this.project.setTitle(title);
-	return this;
-	}
-	
-	public ProjectBuilder setDescription(String description){
-	this.project.setDescription(description);
-	return this;
-	}
-	
-	public ProjectBuilder addUser(User user){
-	this.project.addUser(user);
-	return this;
-	}
-	
-	public ProjectBuilder addIssue(Issue issue){
-	this.project.addIssue(issue);
-	return this;
-	}
-	
+    public ProjectBuilder() {
+        this.project = build();
+    }
+
+    public Project build() {
+        return this.project;
+    }
+
+    public ProjectBuilder setTitle(String title) {
+        this.project.setTitle(title);
+        return this;
+    }
+
+    public ProjectBuilder setDescription(String description) {
+        this.project.setDescription(description);
+        return this;
+    }
+
+    public ProjectBuilder addUser(User user) {
+        this.project.addUser(user);
+        return this;
+    }
+
+    public ProjectBuilder addIssue(Issue issue) {
+        this.project.addIssue(issue);
+        return this;
+    }
+
 }
 
