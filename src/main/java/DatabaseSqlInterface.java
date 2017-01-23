6753f28b97319531f5a7349221cf7390dfe8d1a4
Bartłomiej Dziwoń <dziwon.bartlomiej@gmail.com>
2017-01-23 20:46:24 +0100

    Zaktualizowano interfejs

--- src/main/java/DatabaseSqlInterface.java
+++ src/main/java/DatabaseSqlInterface.java
@@ -1,8 +1,13 @@
+import java.sql.ResultSet;
 
+/**
+ * Created by Bartłomiej Dziwoń on 22.01.2017.
+ */
 public interface DatabaseSqlInterface {
     public String makeUpdateSql();
     public String makeDeleteSql();
     public String makeInsertSql();
     public String makeSelectSql();
     public int setId(int id);
+    public Object resultSetToObject(ResultSet resultSet);
 }
