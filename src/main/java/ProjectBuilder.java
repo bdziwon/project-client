756761a1782875716bb6554d21e020c1f3b6211f
mikolajczaja <pridak136@op.pl>
2017-01-23 20:49:40 +0100

    wiecej poprawek

--- src/main/java/ProjectBuilder.java
+++ src/main/java/ProjectBuilder.java
@@ -3,21 +3,14 @@ public class ProjectBuilder {
 
 	private Project project;
 	
-<<<<<<< HEAD
+
 	public Project build(){
-=======
-	public Project Build(){
->>>>>>> 1be31d98e8c135d647d63dadaae99e6ccb2646cd
 	Project project= new Project();
 	return this.project;
 	}
 	
 	public ProjectBuilder(){
-<<<<<<< HEAD
 	this.project=build();	
-=======
-	this.project=Build();	
->>>>>>> 1be31d98e8c135d647d63dadaae99e6ccb2646cd
 	}
 	
 	public ProjectBuilder setTitle(String title){
