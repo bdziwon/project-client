94d2f0610e0eaa369c0e5ba5f528fcdfdf9ce75d
Bartłomiej Dziwoń <xpxpria@gmail.com>
2017-01-31 17:31:38 +0100

    Dodano pustą scenę główną

---
+++ src/main/java/scenes/OverviewScene.java
@@ -0,0 +1,14 @@
+package scenes;
+
+import controllers.NavigationController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import util.Credentials;
+import util.User;
+import util.facades.UserFacade;
+
+
+public class OverviewScene {
+
+}
